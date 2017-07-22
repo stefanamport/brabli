a423128823ec07f5ec825c6ceb74322dd0a70c75
Stefan Amport <privat@stefanamport.ch>
2017-07-22 16:08:43 +0200

    app components & typewriter2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <searchfield></searchfield>
+    <textlist></textlist>
   </div>
 </template>
 
 <script>
+import Searchfield from './Searchfield.vue'
+import Textlist from './Textlist.vue'
+
 export default {
   name: 'app',
+  components : { Searchfield, Textlist },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
+
+$global-padding: 40px;
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+  padding: 2px;
+  margin: 0px;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  font-size: 100%;
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  color: #fff;
 
-a {
-  color: #42b983;
+  background: #3CA55C;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #B5AC49, #3CA55C);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #B5AC49, #3CA55C); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 }
 </style>
